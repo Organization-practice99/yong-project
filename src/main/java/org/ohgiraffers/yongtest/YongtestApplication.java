553ad95dded9aa34d4b2a725c8e244f9a41eb5dd
DESKTOP-68QM4EC\hi <sy99639@naver.com>
2024-02-16 12:01:57 +0900

    yong commmit

--- src/main/java/org/ohgiraffers/yongtest/YongtestApplication.java
+++ src/main/java/org/ohgiraffers/yongtest/YongtestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class YongtestApplication {
 
     public static void main(String[] args) {
+        System.out.println("yong test");
         SpringApplication.run(YongtestApplication.class, args);
     }
 
