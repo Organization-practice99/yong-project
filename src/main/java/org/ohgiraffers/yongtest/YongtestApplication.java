ac58992005bfa795865de4abd15c7d3c7bf8d5d9
DESKTOP-68QM4EC\hi <sy99639@naver.com>
2024-02-16 11:40:52 +0900

    yong commmit

---
+++ src/main/java/org/ohgiraffers/yongtest/YongtestApplication.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.yongtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YongtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YongtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ohgiraffers/yongtest/YongtestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.yongtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YongtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
